20570e376c9ffbabdc23e65545d1c0c29b73c475
Mike Dinh <mike@Mikes-MacBook-Pro-10.local>
2016-03-26 13:03:40 -0500

    initial push

---
+++ ahoy.js
@@ -0,0 +1,33 @@
+// Load the twilio module
+var twilio = require('twilio');
+var TWILIO_ACCOUNT_SID = 'AC793c293ad8fcd487c62bd0fc7c84f6a5';
+var TWILIO_AUTH_TOKEN = '188b0921e1e8a92ab4178a6df4fca025';
+
+// Create a new REST API client to make authenticated requests against the
+// twilio back end
+var client = new twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+// Pass in parameters to the REST API using an object literal notation. The
+// REST client will handle authentication and response serialzation for you.
+client.sms.messages.create({
+    to:'+15133739473',
+    from:'+18597400088',
+    body:'Are you Mike Dinh?'
+}, function(error, message) {
+    // The HTTP request to Twilio will run asynchronously. This callback
+    // function will be called when a response is received from Twilio
+    // The "error" variable will contain error information, if any.
+    // If the request was successful, this value will be "falsy"
+    if (!error) {
+        // The second argument to the callback will contain the information
+        // sent back by Twilio for the request. In this case, it is the
+        // information about the text messsage you just sent:
+        console.log('Success! The SID for this SMS message is:');
+        console.log(message.sid);
+
+        console.log('Message sent on:');
+        console.log(message.dateCreated);
+    } else {
+        console.log('Oops! There was an error.');
+    }
+});
